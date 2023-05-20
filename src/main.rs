1904c8dfde780202cc24524b0aa1ee4c9f0b58ae
elaleman <elalemanx@example.com>
2023-05-20 16:40:49 -0300

    establish tls connection with the client

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    path::Path,
+    sync::Arc,
+};
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
+use tokio_rustls::{
+    rustls::{self, Certificate, PrivateKey},
+    server::TlsStream,
+    TlsAcceptor,
+};
+
+use rustls_pemfile::{certs, read_one, rsa_private_keys, Item};
+
 #[tokio::main]
 async fn main() {
+    let certs = load_certs(&Path::new(
+        "/home/elaleman/workspace/efin-proxy/files/certificate.pem",
+    ))
+    .unwrap();
+    eprintln!("certs: {certs:?}");
+
+    let key = match read_one(&mut BufReader::new(
+        File::open("/home/elaleman/workspace/efin-proxy/files/key.pem").unwrap(),
+    ))
+    .unwrap()
+    .unwrap()
+    {
+        Item::PKCS8Key(key) => PrivateKey(key),
+        _ => panic!("invalid key"),
+    };
+
+    eprintln!("key: {key:?}");
+
+    let config = rustls::ServerConfig::builder()
+        .with_safe_defaults()
+        .with_no_client_auth()
+        .with_single_cert(certs, key)
+        .map_err(|err| io::Error::new(io::ErrorKind::InvalidInput, err))
+        .unwrap();
+    let acceptor = TlsAcceptor::from(Arc::new(config));
+
     let port = 8888;
 
     let listener = TcpListener::bind(format!("127.0.0.1:{port}"))
@@ -16,13 +57,15 @@ async fn main() {
         let (stream, _) = listener.accept().await.unwrap();
         eprintln!("INFO: connection established");
 
+        let acceptor = acceptor.clone();
+
         tokio::spawn(async move {
-            handle_connection(stream).await;
+            handle_connection(stream, acceptor).await;
         });
     }
 }
 
-async fn handle_connection(mut stream: TcpStream) {
+async fn handle_connection(mut stream: TcpStream, acceptor: TlsAcceptor) {
     const BUF_SIZE: usize = 1024; // TODO: select a better value
     let mut buf = vec![0; BUF_SIZE];
     let mut index = 0;
@@ -61,7 +104,9 @@ async fn handle_connection(mut stream: TcpStream) {
                 let response = "HTTP/1.1 200 OK\r\n\r\n";
                 stream.write_all(response.as_bytes()).await.unwrap();
 
-                tunnel_data(stream, dest_stream).await;
+                let client_stream = acceptor.accept(stream).await.unwrap();
+
+                tunnel_data(client_stream, dest_stream).await;
             } else {
                 eprintln!("INFO: not a CONNECT request");
                 eprintln!("{req:?}");
@@ -74,12 +119,22 @@ async fn handle_connection(mut stream: TcpStream) {
     }
 }
 
-async fn tunnel_data(mut client_stream: TcpStream, mut dest_stream: TcpStream) {
-    let (mut client_reader, mut client_writer) = client_stream.split();
-    let (mut dest_reader, mut dest_writer) = dest_stream.split();
+async fn tunnel_data(mut client_stream: TlsStream<TcpStream>, mut dest_stream: TcpStream) {
+    let mut buf = [0; 4096];
+    let bytes_read = client_stream.read(&mut buf).await.unwrap();
 
-    let client_to_dest = tokio::io::copy(&mut client_reader, &mut dest_writer);
-    let dest_to_client = tokio::io::copy(&mut dest_reader, &mut client_writer);
+    eprintln!("INFO: read {bytes_read} bytes from client");
+    eprintln!("data: ->{}<-", String::from_utf8_lossy(&buf[..bytes_read]));
+}
 
-    let _ = tokio::try_join!(client_to_dest, dest_to_client);
+fn load_certs(path: &Path) -> io::Result<Vec<Certificate>> {
+    certs(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid cert"))
+        .map(|mut certs| certs.drain(..).map(Certificate).collect())
+}
+
+fn load_keys(path: &Path) -> io::Result<Vec<PrivateKey>> {
+    rsa_private_keys(&mut BufReader::new(File::open(path)?))
+        .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid key"))
+        .map(|mut keys| keys.drain(..).map(PrivateKey).collect())
 }
